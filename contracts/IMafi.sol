301b30e76ed6499f17764248c0c3b7d7ff63fde1
alexzivkovicdev <alexzivkovicdev@gmail.com>
2020-10-02 20:07:45 +0200

    fix

--- contracts/IMafi.sol
+++ contracts/IMafi.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.6.10;
 pragma experimental ABIEncoderV2;
 
 interface IMafi {
-    function delegate(address delegatee) external;
+    function delegate(address delegate_) external;
     function balanceOf(address account) external view returns (uint);
     function transfer(address dst, uint rawAmount) external returns (bool);
     function transferFrom(address src, address dst, uint rawAmount) external returns (bool);
